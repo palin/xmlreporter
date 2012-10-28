97659966073d2d2b7a9e54ba16f52da60c093a47
Maciej Paliński <maciej.palinski@netguru.pl>
2012-10-28 17:45:36 +0100

    basics

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xmlreporter::Application.config.secret_token = 'bb4ba7a4849d3f862963c7fd0699cd37e666ec5185c5a2586a2dba54d6aab5aa547f520690c772235d9b63ab0913c166e23e1b4a02296e127d8cb02fbf78805b'
